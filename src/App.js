2a996db285eb9fc27b3a9a9327227e9a8d8036bc
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-10-06 14:37:08 +0300

    Enable navigation between components
    -Install React Router
    
    -Add Router and paths to home, stations and journeys components
    
    Install react-bootstrap
    
    -Add navigationbar component
    
    -Add folder for pages different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavigationBar from './components/navigation';
 import StationList from './components/stationList';
 import JourneyList from './components/journeyList';
+import HomePage from './pages/homepage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />},
+  { path: "stationpage", element: <StationList />},
+  { path: "journeypage", element: <JourneyList />}
+]);
 
 function App() {
   return (
     <div className="App">
-      <StationList />
-      <JourneyList />
+      <NavigationBar />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/journeypage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import JourneyList from '../components/journeyList';
+
+const JourneyPage = () => {
+    return (
+        <div>
+            <h1>JourneyPage</h1>
+          {/* <JourneyList /> */}
+        </div>
+    )
+}
+
+export default JourneyPage;
