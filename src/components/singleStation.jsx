ae8f858297b047017062dd87bdb6cdaaccd7ff7a
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-10-17 15:39:56 +0300

    Display the 5 most popular return and departure stations for a single station

--- src/components/singleStation.jsx
+++ src/components/singleStation.jsx
@@ -26,8 +26,28 @@ const SingleStation = () => {
             <p>Address: {state.adress}</p>
             <p>City: {state.kaupunki}</p>
             <br/>
+            <h3>Station statistics</h3>
+            <br/>
             <p>Number of departures: {stationCalculations.departures}</p>
             <p>Number of returns: {stationCalculations.returns}</p>
+            <p>Average distance of a journey starting from this station: {stationCalculations.averageDistanceStartingFromStation} km</p>
+            <p>Average distance of a journey ending at this station: {stationCalculations.averageDistanceEndingAtStation} km</p>
+            <br/>
+            <h5>Top 5 Return Stations:</h5>
+            {
+                stationCalculations && stationCalculations.top5ReturnStations &&
+                stationCalculations.top5ReturnStations.map((data, index) => (
+                    <p key={index}>{data.returnStationName}, Number of returns: {data.numberOfReturns}</p>
+                ))
+            } 
+            <br/>
+            <h5>Top 5 Departure Stations:</h5>
+            {
+                stationCalculations && stationCalculations.top5DepartureStations &&
+                stationCalculations.top5DepartureStations.map((data, index) => (
+                    <p key={index}>{data.departureStationName}, Number of departures: {data.numberOfDepartures}</p>
+                ))
+            } 
         </div>
     )
 }
