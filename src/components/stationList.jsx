87dbe0a999b8e549bf42031adc6ae52916f3f474
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-11-06 13:02:52 +0200

    Add search for stations
    -Add input field
    
    -Filter stations based on given search term
    
    -Display filtered stations

--- src/components/stationList.jsx
+++ src/components/stationList.jsx
@@ -5,20 +5,40 @@ import { Link } from 'react-router-dom';
 const StationList = () => {
 
     const [stationData, setStationData] = useState([]);
+    const [searchItem, setSearchItem] = useState("")
+    const [filteredStations, setFilteredStations] = useState([])
  
     useEffect(() => {
         StationService.getAll().then(data => {
             setStationData(data)
+            setFilteredStations(data)
         })
     },[])
+
+    const handleSearchInputChange = (event) => {
+        const searchTerm = event.target.value;
+        setSearchItem(searchTerm)
+   
+        const filteredItems = stationData.filter((station) =>
+        station.nimi.toLowerCase().includes(searchTerm.toLowerCase()))
+
+        setFilteredStations(filteredItems);
+    }
 
     return (
         <div>
             <h1>Station List</h1>
 
-            {stationData.map(data => (
-                <p key={data.id}><Link to={`/stations/${data.id}`} state={{...data}}>{data.nimi}</Link></p>
-            ))}
+            <input
+                type="text"
+                value={searchItem}
+                onChange={handleSearchInputChange}
+                placeholder='Type to search'
+            />
+
+            {filteredStations.map(data => 
+                (<p key={data.id}><Link to={`/stations/${data.id}`} state={{...data}}>{data.nimi}</Link></p>)
+            )}
 
         </div>
     )
