9607ab1eab775f7f80bd532e2d594dc73c8993f2
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-11-21 14:23:36 +0200

    Update Home Page
    -Add images to 'Stations' and 'Journeys' sections
    
    -Update description texts for station and journey pages
    
    -Add space between elements

--- src/components/HomePage/homepage.js
+++ src/components/HomePage/homepage.js
@@ -1,28 +1,46 @@
 import React from "react";
 import Hero from './Hero';
 import { Link } from 'react-router-dom';
-import styles from './homepage.module.css'; // Import the CSS file
+import styles from './homepage.module.css';
 import Button from '../Button';
+import ImageStations from '../../stations.png'
+import ImageJourneys from '../../journeys.png'
 
 const HomePage = () => {
     return (
         <div>
             <Hero />
-            <div className={styles.section}>
-                <h2>About Stations</h2>
-                <p>Brief description about bike stations...</p>
-                <Link to="/stations">
-                    <Button text="View Stations" />
-                </Link>
-            </div>
-            <div className={styles.section}>
-                <h2>About Journeys</h2>
-                <p>Brief description about journeys...</p>
-                <Link to="/journeys">
-                    <Button text="View Journeys" />
-                </Link>
-            </div>
-
+         
+                <div className={styles.stationSection}>
+                    <div className={styles.widthContainer}>
+                        <div className={styles.stationContent}>
+                            <h2 className={styles.head}>Stations</h2>
+                            <p className={styles.para}>Explore Helsinki's City Bike Stations with ease on our Station page. Discover a comprehensive list of bike stations in the capital area and access detailed information about each one.</p>                            
+                            <Link to="/stations">
+                                <Button text="View Stations" />
+                            </Link>
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={ImageStations} alt="Stations" className={styles.stationImage}/>
+                        </div>
+                    </div>
+                </div>
+           
+                <div className={styles.journeySection}>
+                    <div className={styles.widthContainer}>
+                        <div  className={styles.imageContainer}>
+                            <img src={ImageJourneys} alt="Journeys" className={styles.journeyImage}/>
+                        </div>
+                        <div className={styles.journeyContent}>
+                            <h2 className={styles.head}>Journeys</h2>
+                            <p className={styles.para}>Check out the journeys made on Helsinki City Bikes through our Journeys page. Get fascinating and detailed information about cycle routes, their lengths and durations.</p>
+                            <Link to="/journeys">
+                                <Button text="View Journeys" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+          
         </div>
     )
 }
