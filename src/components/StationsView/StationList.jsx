c7dcc80fdb44ac213023ad64d1613b4f11df5b85
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-12-02 14:04:59 +0200

    Style Stations Page and Add Filter Buttons
    -Add two buttons to filter station list by city
    
    -Add ’All Stations’ button to clear input field and filters
    
    -Add CSS module and styling

--- src/components/StationsView/StationList.jsx
+++ src/components/StationsView/StationList.jsx
@@ -3,12 +3,13 @@ import StationService from "../../services/stations";
 import { Link } from 'react-router-dom';
 import Loading from '../Loading';
 import { useErrorBoundary } from 'react-error-boundary'
+import styles from './StationList.module.css';
+import Button from '../Button';
 
 
 const StationList = () => {
 
     const { showBoundary } = useErrorBoundary();
-
     const [stationData, setStationData] = useState([]);
     const [searchItem, setSearchItem] = useState("")
     const [filteredStations, setFilteredStations] = useState([])
@@ -31,29 +32,48 @@ const StationList = () => {
     const handleSearchInputChange = (event) => {
         const searchTerm = event.target.value;
         setSearchItem(searchTerm)
-   
         const filteredItems = stationData.filter((station) =>
         station.nimi.toLowerCase().includes(searchTerm.toLowerCase()))
-
         setFilteredStations(filteredItems);
     }
 
-    return (
-        <div>
-            <h1>Station List</h1>
-            <input
-                type="text"
-                value={searchItem}
-                onChange={handleSearchInputChange}
-                placeholder='Type to search'
-            />
+    const clearInput = () => {
+        setSearchItem("");
+        setFilteredStations(stationData)
+    }
 
-            { loading ? <Loading /> : filteredStations.map(data => 
-                (<p key={data.id}><Link to={`/stations/${data.id}`} state={{...data}}>{data.nimi}</Link></p>)
-            )}
+    const handleCityClick = selectedCity => () => {
+        console.log(selectedCity);
+        const cityFilteredStations = selectedCity ? stationData.filter((station) => station.kaupunki === selectedCity) : stationData;
+        console.log(cityFilteredStations)
+        setFilteredStations(cityFilteredStations);
+    }
+
+    return (
+        <div className={styles.stationsSection}>
+            <div className={styles.leftAlignContainer}>
+                <h1>Stations</h1>
+                <input className={styles.input}
+                    type="text"
+                    value={searchItem}
+                    onChange={handleSearchInputChange}
+                    placeholder='Type to search'
+                />
+                <div>
+                    <Button text="All Stations" backgroundColor="#0174be" color="white" width="126px" margin="0 10px 0 0" onClick={clearInput}/>
+                    <Button text="Helsinki" backgroundColor="#035ba4" color="white" width="126px" margin="0 10px 0 0" onClick={handleCityClick("Helsinki")}/>
+                    <Button text="Espoo" backgroundColor="#035ba4" color="white" width="126px" margin="0" onClick={handleCityClick("Espoo")}/>
+                </div>
+                <div className={styles.linkContainer}>
+                    { loading ? <Loading /> : filteredStations.map(data => 
+                        (<p className={styles.stationLink} key={data.id}>
+                            <Link className={styles.link} to={`/stations/${data.id}`} state={{...data}}>{data.nimi}</Link>
+                        </p>)
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
-
 
 export default StationList;
