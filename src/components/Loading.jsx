904be0b8ea91c5f655d997419157466efdad5468
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-12-07 08:53:02 +0200

    Fix Loading component warning ’Received true for non-boolean attribute’

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <div className="text-center" style={{ marginTop: '7rem', marginBottom: '8rem'}}>
       <p>Loading...</p>
-      <div className="spinner-border" style={{ width: '3rem', height: '3rem'}} text-secondary role="status">
+      <div className="spinner-border" style={{ width: '3rem', height: '3rem'}} text-secondary="true" role="status">
         <span className="sr-only"></span>
       </div>
     </div>
