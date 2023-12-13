c56d703989029eb90cc0a82f4c7d9e99e4d16369
Elina Lepistö <elina.lepisto@student.careeria.fi>
2023-12-13 09:51:00 +0200

    Make 404 page responsive

--- src/errorHandling/RouteNotFound.jsx
+++ src/errorHandling/RouteNotFound.jsx
@@ -8,8 +8,8 @@ export function RouteNotFound() {
     return (
         <div className={styles.errorpage}>
             <img className={styles.image404} src={Image404} alt="404" />
-            <h1>The page you're looking for <br /> seems to be missing</h1>
-            <p>Maybe you'll find what you're looking for here:</p>
+            <h1 className={styles.heading}>The page you're looking for <br /> seems to be missing</h1>
+            <p className={styles.linkSection}>Maybe you'll find what you're looking for here:</p>
             <Link className={styles.link404} to="/">Home</Link>       
             <Link className={styles.link404} to="stations">Stations</Link>
             <Link className={styles.link404} to="journeys">Journeys</Link>
