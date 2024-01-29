3f457b6824229111f892ea58c3cb8c49bc51c720
Elina Lepistö <elina.lepisto@student.careeria.fi>
2024-01-29 09:21:31 +0200

    Fix Error component view by adding navbar and footer
    -Commit that fixed navbar issue caused that the navbar and footer were not in the Error view

--- src/errorHandling/ErrorPage.jsx
+++ src/errorHandling/ErrorPage.jsx
@@ -2,12 +2,16 @@ import { useErrorBoundary } from 'react-error-boundary';
 import ErrorIcon from './ErrorIcon';
 import styles from './ErrorPage.module.css';
 import Button from '../components/Button';
+import NavigationBar from "../components/Navbar";
+import Footer from '../components/Footer';
 
 export function ErrorPage({error}) {
     
     const { resetBoundary } = useErrorBoundary();
     return (
-        <div className={styles.errorpage}>
+        <>
+            <NavigationBar/>
+             <div className={styles.errorpage}>
             <ErrorIcon />
             <br />
             <div>
@@ -15,6 +19,9 @@ export function ErrorPage({error}) {
                 <p>{error.message}</p>
             </div>
             <Button text="Let's try again" onClick={resetBoundary}/>
-        </div>
+            </div>
+            <Footer />
+        </>
+    
     )
 }
